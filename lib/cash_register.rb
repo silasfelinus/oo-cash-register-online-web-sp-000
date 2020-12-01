7085bfb60021e8e5be081d1045b81417cdec03b4
Silas Michael Knight <silasfelinus@gmail.com>
2020-12-01 03:03:40 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -3,14 +3,10 @@ class CashRegister
 
 
 
-  def initialize(total = 0)
+  def initialize(total = 0, discount = 0)
     @total = total
+    @discount = discount
   end
 
-  def self.discount
-    new_register = self.new
-    new_register.discount = 20
-    new_register
-  end
 
 end
