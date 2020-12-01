adad4e9e4d2636831f4d38716aa344fad872abb5
Silas Michael Knight <silasfelinus@gmail.com>
2020-12-01 03:02:04 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,9 +1,17 @@
 class CashRegister
+  attr_accessor :total, :discount
 
 
 
   def initialize(total = 0)
     @total = total
+    @discount = 0
+  end
+
+  def self.discount
+    new_register = self.new
+    new_register.discount = 20
+    new_register
   end
 
 end
